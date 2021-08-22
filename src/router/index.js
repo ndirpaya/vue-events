3580342fc30f063f6a7aeca2af238a5d8dbd0f07
ndirpaya <ndirpaya@penguin>
2021-08-22 15:10:01 +0100

    Renamed Home view to EventList, customized route for EventList, reconfigured & updated About route

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import EventList from "../views/EventList.vue";
+import About from "../views/About.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "EventList",
+    component: EventList,
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: About,
   },
 ];
 
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -11,7 +11,7 @@
 import EventCard from "@/components/EventCard.vue";
 
 export default {
-  name: "Home",
+  name: "EventList",
   components: {
     EventCard,
   },
